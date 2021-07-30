7e167a4b1894a9b1d1492a765089f57b6564c2e4
nguyendminh0101@gmail.com <nguyendminh0101@gmail.com>
2021-07-30 11:24:47 +0700

    1

--- ShopBanHang/Controllers/GiohangController.cs
+++ ShopBanHang/Controllers/GiohangController.cs
@@ -3,12 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using PayPal.Api;
+using ShopBanHang.Models;
 
 namespace ShopBanHang.Controllers
 {
     public class GioHangController : Controller
     {
-        // GET: GioHang
+        
         public ActionResult Index()
         {
             return View();
---
+++ ShopBanHang/Models/PaypalLogger.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.IO;
+
+namespace ShopBanHang.Models
+{
+    public class PaypalLogger
+    {
+        public static string LogDirectryPath = Environment.CurrentDirectory;
+        public static void Log(String messages)
+        {
+            try
+            {
+                StreamWriter strw = new StreamWriter(LogDirectryPath + "\\PaypalError.log", true);
+                strw.WriteLine("{0}--->{1}", DateTime.Now.ToString("MM/dd/yyyy HH:mm:ss"), messages);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+    }
+}
